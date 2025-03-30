0da6b9cdf34d2b8c3fd3f17c5d9e2eabb640172f
KHAN MD <157266962+XdTechPro@users.noreply.github.com>
2025-03-30 14:10:21 +0500

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -40,7 +40,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 ┃◈┃• Allmenu
 ┃◈┃• Ownermenu
 ┃◈┃• Othermenu
-┃◈┃• Logo <text>
+┃◈┃• Logo 
 ┃◈┃• Repo
 ┃◈└───────────┈⊷
 ╰──────────────┈⊷
