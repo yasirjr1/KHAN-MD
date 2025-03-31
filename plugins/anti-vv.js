ed8e84140220d89a661b62ab28443cdc598506f2
KHAN MD <157266962+XdTechPro@users.noreply.github.com>
2025-03-31 20:52:03 +0500

    Update anti-vv.js

--- plugins/anti-vv.js
+++ plugins/anti-vv.js
@@ -3,7 +3,7 @@ const { cmd } = require("../command");
 cmd({
   pattern: "vv",
   alias: ["viewonce", 'retrive'],
-  react: '📤',
+  react: '👾',
   desc: "Owner Only - retrieve quoted message back to user",
   category: "owner",
   filename: __filename
