7aef235875da9fd1496c32ef354b6c57d7498e90
KHAN MD <157266962+XdTechPro@users.noreply.github.com>
2025-04-08 08:12:03 +0500

    Update index.js

--- index.js
+++ index.js
@@ -180,6 +180,7 @@ const port = process.env.PORT || 9090;
   const q = args.join(' ')
   const text = args.join(' ')
   const isGroup = from.endsWith('@g.us')
+  const isChannel = from.endsWith('@newsletter');  	  
   const sender = mek.key.fromMe ? (conn.user.id.split(':')[0]+'@s.whatsapp.net' || conn.user.id) : (mek.key.participant || mek.key.remoteJid)
   const senderNumber = sender.split('@')[0]
   const botNumber = conn.user.id.split(':')[0]
