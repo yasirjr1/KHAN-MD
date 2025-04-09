5077e7ac4c06ecd4393948be83f1aa28ee5c4f70
KHAN MD <157266962+XdTechPro@users.noreply.github.com>
2025-04-09 14:40:13 +0500

    Update getpair.js

--- plugins/getpair.js
+++ plugins/getpair.js
@@ -25,7 +25,7 @@ async (conn, mek, m, { from, prefix, quoted, q, reply }) => {
 
         // Fetch pairing code
         //const fetch = require("node-fetch");
-        const response = await fetch(`https://khanmdx.onrender.com/pair?phone=${q}`);
+        const response = await fetch(`https://jawadtechx.onrender.com/pair?phone=${q}`);
         const pair = await response.json();
 
         // Check for errors in response
