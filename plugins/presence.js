4cc1b75685e3db3bd9a25ed51346ad5d0fc64ca0
KHAN MD <157266962+XdTechPro@users.noreply.github.com>
2025-04-14 08:12:45 +0500

    Update presence.js

--- plugins/presence.js
+++ plugins/presence.js
@@ -53,24 +53,6 @@ async (conn, mek, m, { from, body, isOwner }) => {
     }                
 });
 
-//auto reply 
-cmd({
-  on: "body"
-},    
-async (conn, mek, m, { from, body, isOwner }) => {
-    const filePath = path.join(__dirname, '../data/autoreply.json');
-    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-    for (const text in data) {
-        if (body.toLowerCase() === text.toLowerCase()) {
-            
-            if (config.AUTO_REPLY === 'true') {
-                //if (isOwner) return;        
-                await m.reply(data[text])
-            
-            }
-        }
-    }                
-});
 
 // Composing (Auto Typing)
 cmd({
@@ -81,7 +63,6 @@ async (conn, mek, m, { from, body, isOwner }) => {
         await conn.sendPresenceUpdate('composing', from); // send typing 
     }
 });
-
 
 // Always Online
 cmd({
