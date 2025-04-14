9faeb3e9a214b047000ca44a88d9b37b7fb3037c
KHAN MD <157266962+XdTechPro@users.noreply.github.com>
2025-04-14 09:26:43 +0500

    Update auto-voice.js

--- plugins/auto-voice.js
+++ plugins/auto-voice.js
@@ -1,1 +1,26 @@
+const axios = require('axios');
+const config = require('../config');
+const { cmd } = require('../command');
 
+cmd({ on: "body" }, async (conn, mek, m, { from, body, isOwner }) => {
+  try {
+    const url = 'https://raw.githubusercontent.com/XdTechPro/KHAN-DATA/main/autosticker/autovoice.json';
+    const { data } = await axios.get(url);
+
+    for (const text in data) {
+      if (body.toLowerCase() === text.toLowerCase()) {
+        if (config.AUTO_VOICE === 'true') {
+          //if (isOwner) return;
+          await conn.sendPresenceUpdate('recording', from);
+          await conn.sendMessage(from, {
+            audio: { url: data[text] },
+            mimetype: 'audio/mpeg',
+            ptt: true
+          }, { quoted: mek });
+        }
+      }
+    }
+  } catch (err) {
+    console.error("AutoVoice Error:", err.message);
+  }
+});
