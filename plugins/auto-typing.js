bb87372f43b5646f9a813805140c6ffff1fd962c
KHAN MD <157266962+XdTechPro@users.noreply.github.com>
2025-04-15 10:04:40 +0500

    Update auto-typing.js

--- plugins/auto-typing.js
+++ plugins/auto-typing.js
@@ -1,9 +1,37 @@
+//---------------------------------------------------------------------------
+//           KHAN-MD  
+//---------------------------------------------------------------------------
+//  ⚠️ DO NOT MODIFY THIS FILE ⚠️  
+//---------------------------------------------------------------------------
+const { cmd, commands } = require('../command');
+const config = require('../config');
+const prefix = config.PREFIX;
 const fs = require('fs');
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, sleep, fetchJson } = require('../lib/functions2');
+const { writeFileSync } = require('fs');
 const path = require('path');
-const config = require('../config')
-const {cmd , commands} = require('../command')
+
+
+cmd({
+    pattern: "auto-typing",
+    description: "Enable or disable auto-typing feature.",
+    category: "settings",
+    filename: __filename
+},    
+async (conn, mek, m, { from, args, isOwner, reply }) => {
+    if (!isOwner) return reply("*📛 ᴏɴʟʏ ᴛʜᴇ ᴏᴡɴᴇʀ ᴄᴀɴ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ!*");
+
+    const status = args[0]?.toLowerCase();
+    if (!["on", "off"].includes(status)) {
+        return reply("*🫟 ᴇxᴀᴍᴘʟᴇ:  .ᴀᴜᴛᴏ-ᴛʏᴘɪɴɢ ᴏɴ*");
+    }
+
+    config.AUTO_TYPING = status === "on" ? "true" : "false";
+    return reply(`Auto typing has been turned ${status}.`);
+});
 
 // Composing (Auto Typing)
+
 cmd({
     on: "body"
 },    
