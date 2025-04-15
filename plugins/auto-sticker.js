4c8b1828e9efd85525c4047f883af00a7de36ae4
KHAN MD <157266962+XdTechPro@users.noreply.github.com>
2025-04-15 08:36:09 +0500

    Create auto-sticker.js

---
+++ plugins/auto-sticker.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const config = require('../config')
+const {cmd , commands} = require('../command')
+
+//auto sticker 
+cmd({
+  on: "body"
+},    
+async (conn, mek, m, { from, body, isOwner }) => {
+    const filePath = path.join(__dirname, '../data/autosticker.json');
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    for (const text in data) {
+        if (body.toLowerCase() === text.toLowerCase()) {
+            
+            if (config.AUTO_STICKER === 'true') {
+                //if (isOwner) return;        
+                await conn.sendMessage(from,{sticker: { url : data[text]},package: 'KHAN-MD'},{ quoted: mek })   
+            
+            }
+        }
+    }                
+});
