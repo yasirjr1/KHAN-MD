61515778d70eb23e0067c0dc09ffccd847d21f4d
KHAN MD <157266962+XdTechPro@users.noreply.github.com>
2025-04-15 22:31:28 +0500

    Update and rename menu.js to menu-old.js

--- plugins/menu-old.js
+++ plugins/menu-old.js
@@ -5,9 +5,9 @@ const { runtime } = require('../lib/functions');
 const axios = require('axios');
 
 cmd({
-    pattern: "menu2",
+    pattern: "menu3",
     desc: "menu the bot",
-    category: "menu2",
+    category: "menu3",
     react: "⚡",
     filename: __filename
 }, 
